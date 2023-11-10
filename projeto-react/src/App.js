a5a1098456d3d048c9dd84c4982e4da1e974a3bc
FellipeCastro <fellipecastro27d@gmail.com>
2023-11-10 17:46:35 -0300

    aula 06 finalizada

--- projeto-react/src/App.js
+++ projeto-react/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import SayMyName from './components/SayMyName.js';
 import Pessoa from './components/Pessoa.js';
+import Frase from './components/Frase.js';
 
 function App() {
   const nome = "João"
@@ -8,6 +9,8 @@ function App() {
   return (
     <div className="App">
       <h1>Olá mundo</h1>
+      <Frase />
+      <Frase />
       <SayMyName nome="Fellipe" />
       <SayMyName nome={nome} />
       <Pessoa nome="Fellipe" idade="15" profissao="Programador" foto="https://via.placeholder.com/150"/>
