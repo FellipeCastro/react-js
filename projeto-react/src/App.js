c5f22b12d879bd6837bcf4dc6af0d86071c348af
FellipeCastro <fellipecastro27d@gmail.com>
2023-11-10 18:36:12 -0300

    aula 09 finalizada

--- projeto-react/src/App.js
+++ projeto-react/src/App.js
@@ -1,26 +1,16 @@
 import './App.css';
-import SayMyName from './components/SayMyName.js';
-import Pessoa from './components/Pessoa.js';
-import Frase from './components/Frase.js';
-import List from './components/list.js';
+import Evento from './components/Evento';
+import Form from './components/Form';
 
 function App() {
   const nome = "João"
  
   return (
     <div className="App">
-      <h1>Olá mundo</h1>
-      <Frase />
-      <Frase />
-      <SayMyName nome="Fellipe" />
-      <SayMyName nome={nome} />
-      <Pessoa 
-        nome="Fellipe" 
-        idade="15" 
-        profissao="Programador" 
-        foto="https://via.placeholder.com/150"
-      />
-      <List />
+      <h1>Testando eventos</h1>
+      <Evento numero="1"/>
+      <Evento numero="2"/>
+      <Form/>
     </div>
   );
 }
