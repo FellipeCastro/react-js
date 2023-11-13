54f80755e79a52cee8c17e746a5af062dc5dea4d
FellipeCastro <fellipecastro27d@gmail.com>
2023-11-13 16:04:26 -0300

    aula 16 finalizada

--- projeto-react/src/components/layout/footer.js
+++ projeto-react/src/components/layout/footer.js
@@ -1,5 +1,17 @@
+import { FaFacebook, FaInstagram , FaLinkedin} from 'react-icons/fa';
+import styles from './footer.module.css';
+
 function Footer() {
-    return <footer>Rodapé</footer>
+    return (
+    <footer>
+        <ul className={styles.social_list}>
+            <li><FaFacebook/></li>
+            <li><FaInstagram/></li>
+            <li><FaLinkedin/></li>
+        </ul>
+        <p>Nosso Rodapé</p>
+    </footer>
+    )
 }
 
 export default Footer
