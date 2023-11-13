9fab6510111eb18c26e96990d3cd06876a1a672a
FellipeCastro <fellipecastro27d@gmail.com>
2023-11-13 11:44:59 -0300

    aula 11 finalizada

--- projeto-react/src/components/Evento.js
+++ projeto-react/src/components/Evento.js
@@ -1,12 +1,19 @@
-function Evento({numero}) {
+import Button from "./Button";
+
+function Evento() {
     function meuEvento() {
-        console.log(`Fui ativado ${numero}`)
+        console.log(`Fui ativado`)
     }
     
+    function segundoEvento() {
+        console.log('Ativando o segundo evento')
+    }
+
     return (
         <div>
             <p>Clique para disparar um evento: </p>
-            <button onClick={meuEvento} >Ativar</button>
+            <Button event={meuEvento} text="Primeiro Evento"/>
+            <Button event={segundoEvento} text="Segundo Evento"/>
         </div>
     )
 }
